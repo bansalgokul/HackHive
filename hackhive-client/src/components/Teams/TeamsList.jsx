3e87633f504011a08e09263ac09200883eb350df
Gokul Bansal <bansalgokul134@gmail.com>
2024-02-03 13:24:07 +0530

    Updated Teams component

--- hackhive-client/src/components/Teams/TeamsList.jsx
+++ hackhive-client/src/components/Teams/TeamsList.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react"
 
-const TeamsList = ({ teams }) => {
-  return (
-    <div>
-      <h2>List of Teams</h2>
-      <ul>
-        {teams.map((team) => (
-          <li key={team.id}>{team.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const TeamsList = ({ teams = [] }) => {
+	return (
+		<div>
+			<h2>List of Teams</h2>
+			<ul>
+				{teams.map((team) => (
+					<li key={team.id}>{team.name}</li>
+				))}
+			</ul>
+		</div>
+	)
+}
 
-export default TeamsList;
+export default TeamsList
